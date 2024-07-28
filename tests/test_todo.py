0ecb433f6f92fc0af575a3e0a50e844062177396
pouya <pouyapouryaie@gmail.com>
2024-07-28 18:29:40 -0300

    CRUD test

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -17,12 +17,34 @@ def test_create_todo():
     # create Task
     sample_todo = dict(create_sample_task())
     response = create_task(sample_todo)
-    print(response.json())
+    data = response.json()
+    print(data)
 
     # get Task and check value
-    task_id = response.json()["body"]["id"]
-    find_task = client.get(f"/todo/{task_id}")
-    assert find_task.json()['body']['title'] == sample_todo['title']
+    task_id = data["body"]["id"]
+    find_todo_response = get_task(task_id)
+    assert response.status_code == 200
+    todo = find_todo_response.json()
+    assert todo['body']['title'] == sample_todo['title']
+
+def test_update_todo():
+    # create Task
+    sample_todo = create_sample_task()
+    create_todo_response = create_task(dict(sample_todo))
+    assert create_todo_response.status_code == 200
+    data = create_todo_response.json()
+
+    # update Task
+    task_id = data["body"]["id"]
+    sample_todo.description = "this is a new description"
+    update_todo_response = update_task(task_id=task_id, payload=dict(sample_todo))
+    assert update_todo_response.status_code == 200
+
+    # get Task and check value
+    find_todo_response = get_task(task_id)
+    assert find_todo_response.status_code == 200
+    todo = find_todo_response.json()
+    assert todo['body']['description'] == sample_todo.description
 
 def test_delete_todo():
     # create Task
